1ea9d6e8196a06edf9250de0ecf74175ecf66ae0
Tatsunomiya <black.r.d@icloud.com>
2019-12-24 16:46:01 +0900

    完成

--- app/src/main/java/tatsunomiya/com/yonsoku/MainActivity.kt
+++ app/src/main/java/tatsunomiya/com/yonsoku/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
         button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+
+        button4.setOnClickListener(this)
+
+
     }
 
 
@@ -24,6 +32,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when(v.id) {
 
             R.id.button1 -> plus()
+
+            R.id.button2 -> hiku()
+
+            R.id.button3 ->  kake()
+
+            R.id.button4 -> waru()
 
         }
 
@@ -45,6 +59,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val resolve = editText1.getText().toString().toDouble()
             val resolve2 = editText2.getText().toString().toDouble()
 
+            val data = Bundle()
+
+            data.putString("button", "A")
 
 
             Log.d("tashunomiya", "エラーです")
@@ -53,7 +70,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             intent.putExtra("VALUE1", resolve)
             intent.putExtra("VALUE2", resolve2)
-
+            intent.putExtras(data)
 
 
             startActivity(intent)
@@ -62,6 +79,112 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }catch(e: Exception) {
             Log.d("tashunomiya", "エラーです")
             
+
+        }
+
+
+    }
+
+
+    fun hiku(){
+        try {
+
+
+            val resolve = editText1.getText().toString().toDouble()
+            val resolve2 = editText2.getText().toString().toDouble()
+
+            val data = Bundle()
+
+            data.putString("button", "B");
+
+
+            Log.d("tashunomiya", "エラーです")
+
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("VALUE1", resolve)
+            intent.putExtra("VALUE2", resolve2)
+            intent.putExtras(data)
+
+
+
+            startActivity(intent)
+
+
+        }catch(e: Exception) {
+            Log.d("tashunomiya", "エラーです")
+
+
+        }
+
+
+    }
+
+
+    fun kake(){
+        try {
+
+
+            val resolve = editText1.getText().toString().toDouble()
+            val resolve2 = editText2.getText().toString().toDouble()
+
+
+            val data = Bundle()
+
+            data.putString("button", "C");
+
+
+            Log.d("tashunomiya", "エラーです")
+
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("VALUE3", resolve)
+            intent.putExtra("VALUE3", resolve2)
+            intent.putExtras(data)
+
+
+            startActivity(intent)
+
+
+        }catch(e: Exception) {
+            Log.d("tashunomiya", "エラーです")
+
+
+        }
+
+
+    }
+
+
+    fun waru(){
+        try {
+
+
+            val resolve = editText1.getText().toString().toDouble()
+            val resolve2 = editText2.getText().toString().toDouble()
+
+
+
+            val data = Bundle()
+
+            data.putString("button", "D");
+
+
+            Log.d("tashunomiya", "エラーです")
+
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("VALUE1", resolve)
+            intent.putExtra("VALUE2", resolve2)
+            intent.putExtras(data)
+
+
+            startActivity(intent)
+
+
+        }catch(e: Exception) {
+            Log.d("tashunomiya", "エラーです")
+
 
         }
 
--- app/src/main/java/tatsunomiya/com/yonsoku/SecondActivity.kt
+++ app/src/main/java/tatsunomiya/com/yonsoku/SecondActivity.kt
@@ -4,19 +4,41 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
+
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val value1 = intent.getDoubleExtra("VALUE1", 0.1)
-        val value2 = intent.getDoubleExtra("VALUE2", 1.1)
 
-        textView.text = "${value1 + value2}"
+        var button = intent.getStringExtra("button")
+
+
+          val value1 = intent.getDoubleExtra("VALUE1", 5.5)
+          val value2 = intent.getDoubleExtra("VALUE2", 1.1)
+
+        when (button) {
+
+            "A" -> textView.text = "${value1 + value2}"
+
+
+            "B" -> textView.text = "${value1 - value2}"
+
+            "C" -> textView.text = "${value1 * value2}"
+
+
+
+            "D" -> textView.text = "${value1 / value2}"
+
+
+        }
+
+
     }
-
-
-
-
 }
+
+
+
+
+
